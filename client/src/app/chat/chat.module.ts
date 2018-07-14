38879e446822a0658df97675f6a9c263a2dd24b2
Jordan Bowker <jordanbowker90@gmail.com>
2018-07-14 19:25:39 +0100

    Added bootstrap to the project

--- client/src/app/chat/chat.module.ts
+++ client/src/app/chat/chat.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ChatComponent } from './chat.component';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    NgbModule
   ],
   declarations: [ChatComponent]
 })
